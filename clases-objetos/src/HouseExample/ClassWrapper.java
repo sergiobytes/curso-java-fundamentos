d7e74c641c4ba5f15610b24a753e63a43ef1ee68
Sergio Barreras <sergiobg.isc@gmail.com>
2025-09-30 10:20:43 -0600

    Add ClassWrapper class with main method and variable initializations

---
+++ clases-objetos/src/HouseExample/ClassWrapper.java
@@ -0,0 +1,8 @@
+package HouseExample;
+
+public class ClassWrapper {
+    public static void main(String[] args) {
+        int number = 10;
+        Integer number2 = Integer.valueOf(30);
+    }
+}
