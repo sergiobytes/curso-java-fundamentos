81b7d24f4462fa7002bd14a18ef6461f07cb0fcc
Sergio Barreras <sergiobg.isc@gmail.com>
2025-09-30 08:47:55 -0600

    Encapsulate House properties and update HouseMain to use getters and setters

--- clases-objetos/src/HouseExample/House.java
+++ clases-objetos/src/HouseExample/House.java
@@ -1,9 +1,9 @@
 package HouseExample;
 
 public class House {
-    int doors;
-    int windows;
-    double size;
+    private int doors;
+    private int windows;
+    private double size;
 
     public House() {
     }
@@ -14,11 +14,35 @@ public class House {
         this.size = size;
     }
 
-    void openDoor() {
+    public int getDoors() {
+        return doors;
+    };
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public int getWindows() {
+        return windows;
+    };
+
+    public void setWindows(int windows) {
+        this.windows = windows;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public void openDoor() {
         System.out.println("La puerta esta abierta");
     }
 
-    double calculateArea() {
+    public double calculateArea() {
         return size * size;
     }
 }
--- clases-objetos/src/HouseExample/HouseMain.java
+++ clases-objetos/src/HouseExample/HouseMain.java
@@ -2,12 +2,16 @@ package HouseExample;
 
 public class HouseMain {
     public static void main(String[] args) {
-        House myHouse = new House(2, 4, 10);
+        House myHouse = new House();
+        myHouse.setDoors(2);
+        myHouse.setWindows(4);
+        myHouse.setSize(10);
 
         House myHouse2 = new House(4, 6, 20);
 
-        System.out.println("My house has " + myHouse.doors + " doors, " + myHouse.windows + " windows, and is "
-                + myHouse.calculateArea() + " square meters.");
+        System.out
+                .println("My house has " + myHouse.getDoors() + " doors, " + myHouse.getWindows() + " windows, and is "
+                        + myHouse.calculateArea() + " square meters.");
 
         myHouse2.openDoor();
     }
